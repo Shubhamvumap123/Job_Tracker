29bf16de76c45a15a93ed293e791dd22a80b2a66
Shubhamvumap123 <Shubhamvumap@gmail.com>
2026-02-07 10:18:34 +0530

    create method  create , view , update ,delete for manage ticket data

--- Server/src/controller/TicketController.js
+++ Server/src/controller/TicketController.js
@@ -9,7 +9,34 @@ const createTicket = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
-
+const getTicketList = async (req, res) => {
+    try {
+        const tickets = await Ticket.find();
+        res.status(200).json(tickets);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+const updateTicket = async (req, res) => {
+    try {
+        const { title, description, priority, status } = req.body;
+        const ticket = await Ticket.findByIdAndUpdate(req.params.id, { title, description, priority, status });
+        res.status(200).json(ticket);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+const deleteTicket = async (req, res) => {
+    try {
+        const ticket = await Ticket.findByIdAndDelete(req.params.id);
+        res.status(200).json(ticket);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 module.exports = {
-    createTicket
+    createTicket,
+    getTicketList,
+    updateTicket,
+    deleteTicket
 };
--- Server/src/routes/TicketRoutes.js
+++ Server/src/routes/TicketRoutes.js
@@ -3,7 +3,20 @@ const express = require("express");
 const router = express.Router();
 
 const { createTicket } = require("../controller/TicketController");
+const { getTicketList } = require("../controller/TicketController");
+const { updateTicket } = require("../controller/TicketController");
+const { deleteTicket } = require("../controller/TicketController");
+// Create tickets 
+
+// View ticket list 
+
+// Update ticket status 
+
+// Delete tickets 
 
 router.post("/create", createTicket);
+router.get("/list", getTicketList);
+router.put("/update/:id", updateTicket);
+router.delete("/delete/:id", deleteTicket);
 
 module.exports = router;
